dd54002be38913162fd9cb234d595759e448b8c9
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-08 10:40:34 -0400

    first push with all relevent info in home page, constructed squat page, constructing bench and deadlift pages next.

---
+++ Form4Life/Controllers/SquatController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form4Life.Controllers
+{
+    public class SquatController : Controller
+    {
+        // GET: Squat
+        public ActionResult SquatForm()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Form4Life/Controllers/SquatFormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Form4Life.Models;
+
+namespace Form4Life.Controllers
+{
+    public class SquatFormController : Controller
+    {
+        // GET: SquatForm
+        public ActionResult Form()
+        {
+            ViewBag.Message = "How to squat 101";
+            var squat = new Squat() {Form = "High Bar!"};
+            return View(squat);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
