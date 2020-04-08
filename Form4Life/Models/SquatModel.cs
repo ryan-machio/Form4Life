05de3055305b3d6f77aad346300d06ee52f02191
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-08 19:56:32 -0400

    comitting edits to layout

---
+++ Form4Life/Models/SquatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form4Life.Models
+{
+    public class Squat
+    {
+        public int Id { get; set; }
+        public string Form { get; set; }
+
+        // /squat/form
+    }
+}
